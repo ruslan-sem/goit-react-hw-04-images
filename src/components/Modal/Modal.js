54346bc62d86a01876b266f8579d2308d7c259c9
Ruslan Samoylenko <ruslan.sem@gmail.com>
2023-02-08 12:53:38 +0200

    Fix final

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -36,39 +36,6 @@ export const Modal = ({ bigImg, switchModal }) => {
   );
 };
 
-// export class Modal extends Component {
-//   handleEsc = event => {
-//     if (event.code === 'Escape') {
-//       this.props.switchModal(event);
-//     }
-//   };
-
-//   handleClickOverlay = event => {
-//     if (event.target === event.currentTarget) {
-//       this.props.switchModal(event);
-//     }
-//   };
-
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleEsc);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleEsc);
-//   }
-
-//   render() {
-//     return createPortal(
-//       <div className={css.Overlay} onClick={this.handleClickOverlay}>
-//         <div className={css.Modal}>
-//           <img src={this.props.bigImg} alt="Big Immage" />
-//         </div>
-//       </div>,
-//       rootModal
-//     );
-//   }
-// }
-
 Modal.propTypes = {
   bigImg: PropTypes.string.isRequired,
   switchModal: PropTypes.func.isRequired,
